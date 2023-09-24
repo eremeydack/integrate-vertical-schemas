0bceea221815b3175745061b9884b09e5e9758fb
eremeydack <139877570+eremeydack@users.noreply.github.com>
2023-09-24 20:03:45 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,69 +1,27 @@
-if __name__ == "__main__":
 
-def main():
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
+    main()
 def generate_random_data():
+    for item in data:
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def main():
     return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+import random
+def main():
+    for item in data:
+
+
 
     data = generate_random_data()
 
     main()
-    for item in data:
-    for item in data:
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-def main():
-
-if __name__ == "__main__":
-import random
-
-    main()
-    data = generate_random_data()
 def generate_random_data():
-
-
-    return data
-    main()
-if __name__ == "__main__":
-import random
-def main():
-def generate_random_data():
-    for item in data:
-    data = generate_random_data()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-        print(f"Random Number: {item}")main();
-}
-
-        console.log("Random Number: " + item);
-    }
-function generateRandomData() {
-    const data = generateRandomData();
-
-    for (let i = 0; i < 10; i++) {
-
-function main() {
-    data.forEach(item => {
-}
-    return data;
-    const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
-    });    data = generate_random_data()
-    for item in data:
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-
-
     return data
     data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-
-
-    main()
-import random
-def main():
